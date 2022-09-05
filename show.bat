496bb9321f312f2c5e3692c37d9c4966ed2c19c3
shapaness <steelex105@gmail.com>
2022-09-05 23:02:52 +0200

    j ai ajoute la commande show pour montrer tout les channels existants

---
+++ show.bat
@@ -0,0 +1,6 @@
+@echo off
+@powershell write-host -fore Cyan Recently joined channels
+@git branch
+@powershell write-host -fore Cyan All channels
+@git branch -r
+@powershell write-host -fore Cyan Use join command to join one of them
